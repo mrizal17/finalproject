8146765366a5d30a6e547fa301e8c7ff66cbeb6c
Muhammad Rizal H <muhammadrizalh45@gmail.com>
2024-11-19 16:05:51 +0700

    login toast

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const [error, setError] = useState("");
